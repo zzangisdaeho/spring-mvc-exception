cc0253f1561d518d4bde8dd1bc9cf21e193bdda8
DaeHo Kim <zzangisdaeho@naver.com>
2022-02-28 03:18:09 +0900

    first commit

---
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -0,0 +1,22 @@
+package hello.exception;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+//@Component
+public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+
+        // 서블릿까지 예외상황이 흘러가면 지정된 PATH의 컨트롤러를 호출한다.
+        // WAS -> 필터 -> 서블릿 -> 인터셉터 -> 컨트롤러(지정 PATH) 가 재실행된다
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
+
+        factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
+    }
+}
